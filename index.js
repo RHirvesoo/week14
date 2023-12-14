7c2b282d607fda4e5f3220a396b95860800e095a
Rasmus <rasmus.hirvesoo@tktk.ee>
2023-12-14 15:07:11 +0200

    error

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log('Hello from script');
+
+/* getRandomRecipe();
+
+async function getRandomRecipe(){
+    const response = await fetch('https://myrecipes1.onrender.com/random');
+    const recipe = await response.json();
+    console.log(recipe);
+} */
