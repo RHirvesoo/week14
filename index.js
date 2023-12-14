822c8b8a7701eaa8ac00693f1f3aec83b114e6c3
Rasmus <rasmus.hirvesoo@tktk.ee>
2023-12-14 16:15:36 +0200

    week14

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
-console.log('Hello from script');
+const containerElement = document.querySelector('.container');
 
-/* getRandomRecipe();
+
+getRandomRecipe();
 
 async function getRandomRecipe(){
     const response = await fetch('https://myrecipes1.onrender.com/random');
     const recipe = await response.json();
-    console.log(recipe);
-} */
+
+    const recipeName = recipe.recipe.recipename;
+    const imageUrl = recipe.recipe.imageurl;
+    const recipeInstructions = recipe.recipe.instructions;    
+    const ingredientsArray = recipe.ingredients;
+
+    const recipeNameTitle = document.createElement('h2');
+    const recipeImage = document.createElement('img');
+    const instructionsBlock = document.createElement('p');
+    const ingredientsList = document.createElement('ul');
+
+    ingredientsArray.forEach(ingredient => {
+        const ingredientLIElement = document.createElement('li');
+        ingredientLIElement.innerHTML = ingredient;
+        ingredientsList.appendChild(ingredientLIElement);
+    });
+
+    recipeNameTitle.innerHTML = recipeName;
+    recipeImage.src = imageUrl;
+    instructionsBlock.innerHTML = recipeInstructions;
+
+
+    const footer = document.createElement('footer');
+    footer.innerHTML = '&#169 2023 IAmCheff All Rights Reserved';
+
+    containerElement.appendChild(recipeNameTitle);
+    containerElement.appendChild(recipeImage);
+    containerElement.appendChild(ingredientsList);
+    containerElement.appendChild(instructionsBlock);
+    containerElement.appendChild(footer);
+
+    console.log(recipeName);
+    console.log(imageUrl);
+
+}
